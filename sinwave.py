5dec56f81c1045bd7558c0df06db5cdcd155497b
mesaket <46367263+mesaket@users.noreply.github.com>
2019-01-03 23:16:30 -0800

    Update sinwave.py

--- sinwave.py
+++ sinwave.py
@@ -1,11 +1,11 @@
 import numpy as np 
 import matplotlib.pyplot as plt  
 
-# Compute the x and y coordinates for points on a sine curve
+#cmputing cordinates
 x = np.arange(0, 3 * np.pi, 0.1) 
 y = np.sin(x) 
 plt.title("sine wave form") 
 
-# Plot the points using matplotli
+# Plottting graph
 plt.plot(x, y) 
 plt.show() 
